077b35633878216094510b8379f5215812496049
mframpton21 <mike521@gmail.com>
2015-01-14 20:07:42 -0700

    Submitting Problem 3 and 4

--- practice.js
+++ practice.js
@@ -1,11 +1,12 @@
 //Once you complete a problem, open up Chrome and check the answer in the console.
 
 
+// Problem 1
+// Create a function called isTyler that accepts name as it's only argument.
+//I f the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 var name = 'Tyler';
-//Create a function called isTyler that accepts name as it's only argument.
-//If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-  //Code Here
+//Code Here
 var isTyler = function(param1) {
   if (param1 === "Tyler") {
     return true;
@@ -18,14 +19,11 @@ var result = isTyler(name);
 alert(result);
 
 
-//Next problem
+// Problem 2
+// Create a function called getName that uses prompt() to prompt the user for their name, 
+// then returns the name.
 
-
-
-//Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
-
-
-  //Code Here
+//Code Here
 var getName = function() {
   var yourName = prompt("What is your name?");
   return yourName;
@@ -34,26 +32,41 @@ var getName = function() {
 var myName = getName();
 alert("My name is " + myName);
 
-//Next Problem
+
+// Problem 3
+// Create a function called welcome that uses your getName function
+// you created in the previous problem to get the users name,
+// then alerts "Welcome, " plus whatever the users name is.
+
+//Code Here
+var welcome = function() {
+  alert("Welcome, " + getName());
+};
+
+var result = welcome();
 
 
+// Problem 4
+// Write a function called adder that uses the 'arguments' keyword that 
+// comes with every function to
+// loop over all the arguments the function received, add them all together, 
+// then return the result.
+// Save the result of the return value from your adder function, then 
+// alert "The Total Number was " + the number that was
+// returned from adder.
 
-//Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
-//then alerts "Welcome, " plus whatever the users name is.
+//Code Here
+var adder = function() {
+  var result = 0;
 
-  //Code Here
+  for (i = 0; i < arguments.length; i++) {
+    result = result + arguments[i];
+  }
+  return result;
 
-
-//Next problem
-
-
-
-//Write a function called adder that uses the 'arguments' keyword that comes with every function to
-//loop over all the arguments the function received, add them all together, then return the result.
-//Save the result of the return value from your adder function, then alert "The Total Number was " + the number that was
-//returned from adder.
-
-  //Code Here
+};
+var number = adder(1, 2, 3, 4, 5, 6);
+alert("The Total Number was " + number);
 
 
 //Next Problem
